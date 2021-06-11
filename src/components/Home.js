a1102304d737adc414d2272609860fe1b1154ce6
Akash Yadav <akashrequired@gmail.com>
2021-06-11 21:48:37 +0530

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Home(props) {
+    return (
+        <div>
+           
+            <h1>Home Component</h1>
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                    <img src="https://images.unsplash.com/photo-1611791485440-24e8239a0377?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="img1"/>
+                </div>
+                <div className="text-wrapper item">
+                    <span>
+                        I-Phone
+                    </span>
+                    <span>
+                        Price: $1000.00
+                    </span>
+                </div>
+                <div className="btn-wrapper item">
+                    <button 
+                    onClick={
+                        ()=>{props.addToCartHandler({pice:1000,name:'i phone 11'})}
+                        }>
+                        Add To Cart</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home
